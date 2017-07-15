f4c0c1bdea027eea2fdccbd9133a17cc476c470b
Taylor Fitzgerald Chesnutt <taylor.chesnutt@gmail.com>
2017-07-15 10:54:04 -0700

    script to login complete

--- chessai/hand/hand.py
+++ chessai/hand/hand.py
@@ -11,11 +11,11 @@ PASSWORD = data['password']
 chromedriver = "/Users/chesnutt/Documents/chessai/chessai/hand/chromedriver"
 driver = webdriver.Chrome(chromedriver)
 driver.get("http://www.chess.com")
-user_ele = driver.find_element_by_name("user_login_username");
-pass_ele = driver.find_element_by_name("user_login_password");
-login_button = driver.find_element_by_name("c2");
-user_ele.sendKeys(USERNAME);
-pass_ele.sendKeys(PASSWORD);
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+user_ele = driver.find_element_by_id("user_login_username")
+pass_ele = driver.find_element_by_id("user_login_password")
+login_button = driver.find_element_by_tag_name('button')
+print(login_button)
+user_ele.send_keys(USERNAME)
+pass_ele.send_keys(PASSWORD)
+login_button.click()
 driver.close()
